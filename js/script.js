852f1ed5a32079f745cc17d7d8e35b367556efc9
kresimir <kresomaric66@gmail.com>
2019-05-26 11:11:10 +0100

    New commit

--- js/script.js
+++ js/script.js
@@ -12,9 +12,6 @@ let nameSection = $('#name-section');
 let blogSection = $('#blog-section');
 let body = $('.container');
 
-let colors = ['#FFEBEE', '#FCE4EC', '#F3E5F5', '#EDE7F6', '#E8EAF6', '#E3F2FD', '#E1F5FE', '#E0F2F1', '#F1F8E9', '#FFF8E1', '#FFFDE7', '#FFF3E0', '#FAFAFA', '#FBE9E7', '#EFEBE9'];
-let randomColors = Math.floor(Math.random() * colors.length);
-
 
 // Hidden about me section on page load
 
@@ -26,7 +23,6 @@ workSection.hide();
 
 // Button to call about section
 aboutBtn.on('click', () => {
-    body.css('background-color', '#' + randomColors);
     nameSection.hide();
     aboutSection.fadeIn();
     workSection.hide();
@@ -34,14 +30,12 @@ aboutBtn.on('click', () => {
 
 // Button to call work section
 workBtn.on('click', () => {
-  body.css('background-color', randomColors);
   nameSection.hide();
   workSection.fadeIn();
   aboutSection.hide();
 });
 
 homeBtn.on('click', () => {
-  body.css('background-color', randomColors);
   nameSection.fadeIn();
   workSection.hide();
   aboutSection.hide();
