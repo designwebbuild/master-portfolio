0a9a4adc954bab3989d7a5c8b64f5448a878f2bb
kresimir <kresomaric66@gmail.com>
2020-01-10 12:29:48 +0000

    Added new contact form

--- js/script.js
+++ js/script.js
@@ -9,19 +9,21 @@ let contactBtn = $('#contact');
 let aboutSection = $('#about-section');
 let workSection = $('#work-section');
 let nameSection = $('#name-section');
-let blogSection = $('#blog-section');
+let contactSection = $('#contact-section');
 let body = $('.container');
 let work = $('.work');
 
 // Hidden about me section on page load
 aboutSection.css('display', 'none');
 workSection.css('display', 'none');
+contactSection.css('display', 'none');
 
 // Button to call about section
 aboutBtn.on('click', () => {
     nameSection.css('display', 'none');
     aboutSection.fadeIn();
     workSection.css('display', 'none');
+    contactSection.css('display', 'none');
 });
 
 // Button to call work section
@@ -29,13 +31,22 @@ workBtn.on('click', () => {
   nameSection.css('display', 'none');
   workSection.fadeIn();
   aboutSection.css('display', 'none');
+  contactSection.css('display', 'none');
 });
 
 homeBtn.on('click', () => {
   nameSection.fadeIn();
   workSection.css('display', 'none');
   aboutSection.css('display', 'none');
+  contactSection.css('display', 'none');
 });
+
+contactBtn.on('click', () => {
+  nameSection.css('display', 'none');
+  aboutSection.css('display', 'none');
+  workSection.css('display', 'none');
+  contactSection.fadeIn();
+})
 
 // darkModeBtn.on('click', () => {
 //   $('html').toggleClass('dark-mode');
